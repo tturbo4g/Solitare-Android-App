e4eb97adde25e07da0399a3fd8ff5f2f2970ece0
Andrew Bryant <andrewpb@vt.edu>
2014-11-17 20:46:43 -0500

    Made new test classes

---
+++ src/com/example/solitare2114/model/tests/DeckTest.java
@@ -0,0 +1,12 @@
+package com.example.solitare2114.model.tests;
+
+public class DeckTest extends student.TestCase
+{
+
+    public void setUp()
+    {
+
+    }
+
+
+}
---
+++ src/com/example/solitare2114/model/tests/RuleTest.java
@@ -0,0 +1,12 @@
+package com.example.solitare2114.model.tests;
+
+// -------------------------------------------------------------------------
+/**
+ *
+ *  @author abryant
+ *  @version Nov 17, 2014
+ */
+public class RuleTest
+{
+
+}
